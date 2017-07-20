666124f168b61416c3e7a59520ccfbb687f893ce
ashley <msashleychen@gmail.com>
2017-07-20 16:47:29 -0400

    Added scp command. Bug found in checkoutFixedProject()

--- diffBugs.py
+++ diffBugs.py
@@ -1,5 +1,9 @@
 import subprocess
+import os
 import ConfigParser
+import re
+from filecmp import dircmp
+
 configs = {}
 
 """
@@ -13,31 +17,79 @@ def readConfigurations():
     configs["version"] = config.get("Basic","version")
     configs["examplesPath"] = config.get("Basic","examplesPath")
     configs["srcPath"] = config.get("Basic","srcPath")
+    configs["copiedFiles"] = config.get("Basic", "copiedFiles")
+    configs["projectPath"] = config.get("Basic", "projectPath")
 
 """
 Parses defect4j info command
-@return {List} filePaths of modified files
+@return {Dictionary} Keys are file names, Values are file paths.
 """
 def defects4jInfo():
-    bashCommand = "defects4j info -p " + project + " -b " + projectNum
+    bashCommand = "defects4j info -p " + configs["project"] + " -b " + configs["projectNum"]
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     #Parsing default defects4j info command
-    fileStrings =  ["%s%s%s" % (srcPath,i[3:].replace('.','/'), ".java") for i in 
+    fileStrings =  ["%s%s%s" % (configs["srcPath"],i[3:].replace('.','/'), ".java") for i in 
                     output.split("--------------------------------------------------------------------------------")
                     [-2].strip('\n').split("\n")[1:]
+    
                     ]
-    return fileStrings
+    fileNames = [i.split('/')[-1] for i in fileStrings]
+    filePackage = dict(zip(fileNames, fileStrings))
+    return filePackage
 
 """
 Checkout fixed project into Example directory
-@param {String} porjectName: name of project from defects4j
-@param {String} versionID: number that identified version of project
 """
-def checkoutFixedProject(projectName, versionID):
-    bashCommand = "defects4j checkout -p " + projectName + " -v " + versionID + "f -w " + examplesPath+projectName + versionID + "F"
+def checkoutFixedProject():
+    bashCommand = "defects4j checkout -p " + configs["project"] + " -v " + configs["projectNum"] + "f -w " + configs["examplesPath"]+configs["projectName"] + configs["projectNum"] + "F"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    configs["fixedPath"] = configs["examplesPath"]+configs["projectName"] + configs["projectNum"] + "F"
+
+"""
+Bash copies modified files to Copies directory
+@param {List} filePaths: list of relative paths
+"""
+def copyModifiedFiles(filePaths):
+    if not os.path.lexists(configs["projectPath"]):
+        os.makedirs(configs["projectPath"])
+
+    for name, path in filePaths.items():
+        buggyCommand =   ''.join([
+                        "scp ", configs["examplesPath"], 
+                        configs["project"].lower(),
+                        configs["projectNum"]+"Buggy/", 
+                        path,
+                        " ",
+                        configs["projectPath"], "/",
+                        configs["projectNum"], "/",
+                        name
+                        ])
+        fixedCommand =   ''.join([
+                        "scp ", configs["fixedPath"] ,"/", 
+                        path,
+                        " ",
+                        configs["projectPath"], "/",
+                        configs["projectNum"], "/",
+                        name
+                        ])
+
+        if not os.path.lexists(configs["projectPath"]+"/"+configs["projectNum"]):
+            os.makedirs(configs["projectPath"]+"/"+configs["projectNum"])
+        if not os.path.lexists(configs["projectPath"]+"/"+configs["projectNum"]+"/b"):
+            os.makedirs(configs["projectPath"]+"/"+configs["projectNum"]+"/b")
+        if not os.path.lexists(configs["projectPath"]+"/"+configs["projectNum"]+"/f"):
+            os.makedirs(configs["projectPath"]+"/"+configs["projectNum"]+"/f")
+
+        process = subprocess.Popen(buggyCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        process = subprocess.Popen(fixedCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+
+
 
 readConfigurations()
-
+checkoutFixedProject()
+copyModifiedFiles(defects4jInfo())
