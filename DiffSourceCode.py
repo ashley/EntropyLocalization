90aa4b192b7c87ec35f3e75ee463dd540e5a229f
ashley <msashleychen@gmail.com>
2017-08-03 03:48:46 -0400

    Fixed DiffSourceCode.py for writing output files

--- DiffSourceCode.py
+++ DiffSourceCode.py
@@ -1,24 +1,34 @@
 import subprocess
 import os, sys
 
-# File where results are stored
-fi = open("Results.txt", "wb")
+"""
+Prepare directory and file for writing
+"""
+def prepareFile():
+    projectName = raw_input("Enter the project Name: ")
+    # File where results are stored
+    if not os.path.lexists("DiffCode"):
+            os.makedirs("DiffCode")
+    fi = open("DiffCode/" + projectName + ".txt", "wb")
+    return fi
 
 """
 Preparation before executing diff command.
+@param {File} fi: File for appending results
 """
-def prepareDirectory():
+def prepareDirectory(fi):
     diffPath = raw_input("Enter absolute path to aggregated source code directory: ")
     versions = [version for version in os.listdir(diffPath) if version != ".DS_Store"] #filter out default mac files
     for versionDirectory in versions:
-        getDiffData(diffPath + "/" + versionDirectory, versionDirectory)
+        getDiffData(diffPath + "/" + versionDirectory, versionDirectory, fi)
 
 """
 Calls diff command but does checks to make sure files exist
 @param {String} versionPath: Absolute path to the version directory
 @param {String} versionID: number that is associated with project
+@param {File} fi: File for appending results
 """
-def getDiffData(versionPath, versionID):
+def getDiffData(versionPath, versionID, fi):
     before_files = [i for i in os.listdir(versionPath + "/b") if i != ".DS_store"]
     after_files = [i for i in os.listdir(versionPath + "/f") if i != ".DS_Store"]
     version_information = {
@@ -31,7 +41,7 @@ def getDiffData(versionPath, versionID):
     count = 0
     if before_files == after_files: #checks if files' b/f versions exist
         for fileName in before_files:
-            appendWorkbook(version_information) #helper function, for easy implementation of Excel sheets
+            appendWorkbook(version_information, fi) #helper function, for easy implementation of Excel sheets
             diffCommand = ''.join([
                 "diff ", 
                 versionPath, "/b/", fileName,  " ",  
@@ -40,14 +50,15 @@ def getDiffData(versionPath, versionID):
             process = subprocess.Popen(diffCommand.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
             bug_information[fileName] = output
-            appendWorkbook(bug_information)
+            appendWorkbook(bug_information,fi)
             count += 1
 
 """
 Currently writes in a file. Can be modified to be used with OpenPyxl
 @param {Dictionary} inputMap: any map with <String,String> for appending to a file
+@param {File} fi: File for appending results
 """
-def appendWorkbook(inputMap):
+def appendWorkbook(inputMap, fi):
     for key, value in inputMap.items():
         fi.write(key + " : " + value + "\n")
 
@@ -60,7 +71,8 @@ def parseOutput(result):
     result[0]
 
 def main():
-    prepareDirectory()
+    resultFile = prepareFile()
+    prepareDirectory(resultFile)
 
 if __name__ == "__main__":
     main()
